26fdf6f999223dc4cd2c0e7184967d1a0e78a4cc
MisterBakka <130415011+MisterBakka@users.noreply.github.com>
2023-09-27 15:16:26 +0200

    tient avec la navbar

--- script.js
+++ script.js
@@ -155,12 +155,3 @@ const formData = {
 
 sendEmail(formData);
 
-document.addEventListener('DOMContentLoaded', function () {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  menuToggle.addEventListener('click', function () {
-    menu.classList.toggle('show-menu');
-  });
-});
-
